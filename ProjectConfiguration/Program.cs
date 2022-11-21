549a890fd85eda83aef0a3bacd16f8ea38eaa0a6
giovxyz98 <giovanni.ardore98@gmail.com>
2022-11-21 10:20:39 +0100

    esercizio

---
+++ ProjectConfiguration/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace ProjectConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
+            Settings settings = config.GetRequiredSection("settings").Get<Settings>();
+
+        }
+        public class Settings
+        {
+            public string SMTP { get; set; }
+            public string POP { get; set; }
+        }
+    }
+}
