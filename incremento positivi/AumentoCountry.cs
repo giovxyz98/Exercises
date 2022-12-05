8df6347ce2fad584ba990e7d630a51d1d3fb5767
giovxyz98 <giovanni.ardore98@gmail.com>
2022-12-05 15:46:04 +0100

    Esercizio Positivi

---
+++ incremento positivi/AumentoCountry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace incremento_positivi
+{
+    public class AumentoCountry:EventArgs
+    {
+        int Pos;
+        public AumentoCountry(int x)
+        {
+            this.Pos += x;
+        }
+
+
+    }
+}
---
+++ incremento positivi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace incremento_positivi
+{
+    public delegate void AumentoPositivi(object obj,AumentoCountry aumento);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Country Italia = new Country("Italia");
+            Italia.AumentoPositivi += new AumentoPositivi(aggiornaPositivi);
+            Italia.Positivi = 230;
+            Console.WriteLine(Italia.Positivi);
+            Italia.Positivi = 500;
+            Console.WriteLine(Italia.Positivi);
+            Console.WriteLine(EU.NumeroPositivi);
+            
+        }
+
+        public static void aggiornaPositivi(object obj,AumentoCountry aumento)
+        {
+            var num = obj.GetType().GetProperty("Positivi").GetValue(obj).ToString();
+            int x=int.Parse(num);
+            EU.NumeroPositivi += x;
+
+        }
+    }
+    public static class EU
+    {
+        public static int NumeroPositivi { get; set; }
+    }
+    public class Country
+    {
+        public string Name { get; set; }
+        public int _positivi=0;
+        public event AumentoPositivi AumentoPositivi;
+
+        public int Positivi { get { return _positivi; }
+            set {
+                if (_positivi != value)
+                {
+                    AumentoCountry AumentoCountry = new AumentoCountry(value);
+                    AumentoPositivi(this,AumentoCountry);
+                    _positivi=value;
+                }
+                    } }
+        
+
+        public Country(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
